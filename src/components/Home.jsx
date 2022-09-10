5e23a7df857f7ed6adfdea2d1f7c71c549fe2f9a
Mohamed Behhar <mbehhar@e3r10p1.1337.ma>
2022-09-10 14:50:18 +0100

    small change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
 			<h1 className="title">I love turning design into Code</h1>
 			<p>I'm Mohamed, a front-end web developer based in Morocco.<br />
 			I really enjoy working with Html, Css, JavaScript and React to creat beautiful and responsive User Interface. <br />
-			Currently, I'm  looking for my first experience as a front-end web developer in ReactJs / NextJs</p>.
+			Currently, I'm  looking for my first experience as a front-end web developer in ReactJs / NextJs.</p>
 			<p className='resume'><a href="/files/resume.pdf" target="_blank" rel="noreferrer">Resume</a>
 			</p>
 		</article>
