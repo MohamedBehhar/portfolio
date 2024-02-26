20403cc37856343dd01edcbcc9f1b1bc4cd05114
mohamed behhar <mohamedbehhar@10.0.0.87>
2024-02-26 19:40:46 +0100

    updating home section and my picture

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,25 +1,36 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-	<div className='home' id='home'>
-		<section className='home-center container'>
-		<div className="img-container">
-			<img src="images/Developer.svg" alt="" className='home-picture'/>
-		</div>
-		<article className='description'>
-			<p className='greeting'>hi, my name is</p>
-			<h1 className="name">Mohamed</h1> 
-			<h1 className="title">I love turning design into Code</h1>
-			<p>I'm Mohamed, a front-end web developer based in Morocco.<br />
-			I really enjoy working with Html, Css, JavaScript and React to create beautiful and responsive User Interface. <br />
-			Currently, I'm  looking for my first experience as a front-end web developer in ReactJs / NextJs.</p>
-			<p className='resume'><a href="/files/resume.pdf" target="_blank" rel="noreferrer">Resume</a>
-			</p>
-		</article>
-		</section>
-	</div>
-  )
-}
+    <div className="home" id="home">
+      <section className="home-center container">
+        <div className="img-container">
+          <img src="images/Developer.svg" alt="" className="home-picture" />
+        </div>
+        <article className="description">
+          <p className="greeting">hi, my name is</p>
+          <h1 className="name">Mohamed</h1>
+          <h1 className="title">I love turning design into Code</h1>
+          Hello there, I'm Mohamed, a passionate front-end web developer hailing
+          from Morocco. <br/> Having successfully navigated my first experience as a
+          front-end developer, I've honed my skills in crafting seamless and
+          visually appealing User Interfaces using HTML, CSS, and JavaScript. I
+          particularly thrive in the world of React, where I find joy in
+          building responsive and engaging web applications. <br/> My journey doesn't
+          stop here; I'm now eagerly seeking new opportunities as a front-end
+          web developer, with a keen interest in working with ReactJS and
+          NextJS. Let's collaborate to bring innovative and captivating digital
+          experiences to life. Feel free to explore my portfolio and get in
+          touch for exciting projects and collaborations!
+          <p className="resume">
+            <a href="/files/resume.pdf" target="_blank" rel="noreferrer">
+              Resume
+            </a>
+          </p>
+        </article>
+      </section>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
