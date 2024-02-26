789ab6c32bd771a1ee5af045a24c17ca40cda351
mohamed behhar <mohamedbehhar@10.0.0.87>
2024-02-26 20:05:25 +0100

    updating description, and changing my picture

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,17 +11,23 @@ const Home = () => {
           <p className="greeting">hi, my name is</p>
           <h1 className="name">Mohamed</h1>
           <h1 className="title">I love turning design into Code</h1>
-          Hello there, I'm Mohamed, a passionate front-end web developer hailing
-          from Morocco. <br/> Having successfully navigated my first experience as a
-          front-end developer, I've honed my skills in crafting seamless and
-          visually appealing User Interfaces using HTML, CSS, and JavaScript. I
-          particularly thrive in the world of React, where I find joy in
-          building responsive and engaging web applications. <br/> My journey doesn't
-          stop here; I'm now eagerly seeking new opportunities as a front-end
-          web developer, with a keen interest in working with ReactJS and
-          NextJS. Let's collaborate to bring innovative and captivating digital
-          experiences to life. Feel free to explore my portfolio and get in
-          touch for exciting projects and collaborations!
+          <p>
+            {" "}
+            Hello there, I'm Mohamed, a passionate front-end web developer
+            hailing from Morocco. <br /> Having successfully navigated my first
+            experience as a front-end developer, I've honed my skills in
+            crafting seamless and visually appealing User Interfaces using HTML,
+            CSS, and JavaScript. I particularly thrive in the world of React,
+            where I find joy in building responsive and engaging web
+            applications.
+            <br /> My journey doesn't stop here; I'm now eagerly seeking new
+            opportunities as a front-end web developer, with a keen interest in
+            working with ReactJS and NextJS. Let's collaborate to bring
+            innovative and captivating digital experiences to life. Feel free to
+            explore my portfolio and get in touch for exciting projects and
+            collaborations!
+          </p>
+
           <p className="resume">
             <a href="/files/resume.pdf" target="_blank" rel="noreferrer">
               Resume
